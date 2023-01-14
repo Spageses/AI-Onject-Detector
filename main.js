b10a58fb65bf01b6321fe6becef91c6b86b2f6cd
Spageses <86549403+Spageses@users.noreply.github.com>
2023-01-14 18:09:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+video = "";
+status = "";
+objects = [];
+
+function setup() {
+    canvas = createCanvas(480, 280);
+    canvas.center();
+    video = createCapture(VIDEO)
+    video.hide();
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    status = true;
+    video.speed(1);
+    video.volume(0);
+    video.loop();
+}
+
+function gotResult(error, result) {
+    if (error) {
+        console.error();
+    }
+    console.log(result);
+    objects = result;
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380); 
+    if (status != "") {
+        objectDetector.detect(video, gotResult);
+        for(i=0; i< objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of Objects Detected are :" + objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            nofill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height );
+        }
+    }
+}
